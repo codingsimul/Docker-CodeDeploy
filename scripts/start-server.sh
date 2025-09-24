1cf1c55f1cf6ed85eaf84a60c5196fe35aaef365
ghkdwnsdlr <imbanana15@gmail.com>
2025-09-24 23:12:31 +0900

    docker codeDeploy-3

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,6 +3,6 @@
 echo "서버 배포 시작"
 docker stop dockerdeploy || true
 docker rm dockerdeploy || true
-docker pull 482268324407.dkr.ecr.ap-northeast-2.amazonaws.com/junig-ecr
-docker run -d --name dockerdeploy -p 8080:8080 482268324407.dkr.ecr.ap-northeast-2.amazonaws.com/junig-ecr
+docker pull 482268324407.dkr.ecr.ap-northeast-2.amazonaws.com/junig-ecr:latest
+docker run -d --name dockerdeploy -p 8080:8080 482268324407.dkr.ecr.ap-northeast-2.amazonaws.com/junig-ecr:latest
 echo "서버 배포 완료"
