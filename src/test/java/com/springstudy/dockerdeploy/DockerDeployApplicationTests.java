7d2cff81576dd11f877f9a0af26c043acd0296d5
ghkdwnsdlr <imbanana15@gmail.com>
2025-09-24 16:18:02 +0900

    first commit

---
+++ src/test/java/com/springstudy/dockerdeploy/DockerDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstudy.dockerdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
